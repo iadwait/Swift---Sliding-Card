29f8d7948667fd512b84c7963ea1885e8c0c1219
iAdwait <adwaitbarkale@gmail.com>
2020-10-27 10:28:36 +0530

    Initial Commit

---
+++ Sliding Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sliding Card
+//
+//  Created by Adwait Barkale on 27/10/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
